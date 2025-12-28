f3c65e9fd142b5688357ec61b420e4c627ea50fb
Rijad Pleho <rijad.pleho@stu.ibu.edu.ba>
2025-12-29 00:19:43 +0100

    .

--- Backend/config.php
+++ Backend/config.php
@@ -1,40 +1,45 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL ^ (E_NOTICE | E_DEPRECATED));
 
-class Config
-{
-    public static function DB_NAME()
-    {
-        return 'icehawks_shop';
+$isRailway = getenv('RAILWAY_ENVIRONMENT') !== false;
+
+
+if ($isRailway) {
+    ini_set('display_errors', 0);
+    ini_set('display_startup_errors', 0);
+    error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
+} else {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL ^ (E_NOTICE | E_DEPRECATED));
+}
+
+class Config {
+
+   
+    public static function DB_NAME() {
+       
+        return getenv('MYSQLDATABASE') ?: 'icehawks_shop';
     }
 
-    public static function DB_PORT()
-    {
-        return 3306;
+    public static function DB_PORT() {
+        return getenv('MYSQLPORT') ?: '3306';
     }
 
-    public static function DB_USER()
-    {
-        return 'root';
+    public static function DB_USER() {
+        return getenv('MYSQLUSER') ?: 'root';
     }
 
-    public static function DB_PASSWORD()
-    {
-        return '';
+    public static function DB_PASSWORD() {
+        return getenv('MYSQLPASSWORD') ?: '';
     }
 
-    public static function DB_HOST()
-    {
-        return '127.0.0.1';
+    public static function DB_HOST() {
+        return getenv('MYSQLHOST') ?: '127.0.0.1';
     }
 
-
-    public static function JWT_SECRET()
-    {
-        return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30';
+  
+    public static function JWT_SECRET() {
+        return getenv('JWT_SECRET') ?: 'ICEHAWKS_DEV_SECRET';
     }
 }
-?>
